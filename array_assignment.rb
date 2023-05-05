e4c65b658b871e54b7a2907afac9294b7d1f3e20
347340 <3473401075@qq.com>
2023-05-05 10:11:52 +0800

    ruby数组（array）

---
+++ array_assignment.rb
@@ -0,0 +1,3 @@
+# 可以给数组中的每个元素赋值
+names = Array.new(4, "mac")
+puts "#{names}"
---
+++ array_create.rb
@@ -0,0 +1,5 @@
+# 有多种方式创建或初始化数组。一种方式是通过 new 类方法
+# 数组 names 的大小或长度为 20 个元素。您可以使用 size 或 length 方法返回数组的大小
+names = Array.new(20)
+puts names.size # 返回20
+puts names.length # 返回20
